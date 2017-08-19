54413459738c5a777b42bd485f40cf7838c4e192
CBSkarmory <CBSkarmory@users.noreply.github.com>
2017-08-19 13:53:27 -0400

    refactoring

--- urlfix.js
+++ urlfix.js
@@ -3,20 +3,21 @@ const pattern_0 = "https://l.facebook.com/l.php?u=";
 function fix_urls() {
     var anchors = document.getElementsByTagName("a");
     for(var i = 0; i < anchors.length; i++) {
-        if(anchors[i].href.indexOf(pattern_0) === 0) {
-            //anchors[i].onmouseover();
-            //alert("found one");
-            if(null != anchors[i].onclick) {
-                anchors[i].onmouseover();
-                //FF
-                anchors[i].setAttribute('onclick','#');
-                anchors[i].setAttribute('onmouseover','#');
-                anchors[i].setAttribute('tabindex','#');
-                anchors[i].setAttribute('target','#');
-                anchors[i].setAttribute('rel','#');
-                console.log("[UFX] cleaned link: "+anchors[i].href);
-            }
-            
+        filter_0(anchors, i);
+    }
+}
+
+function filter_0(anchors, i) {
+    if(anchors[i].href.indexOf(pattern_0) === 0) {
+        if(null != anchors[i].onclick) {
+            anchors[i].onmouseover();
+            //FF
+            anchors[i].setAttribute('onclick','#');
+            anchors[i].setAttribute('onmouseover','#');
+            anchors[i].setAttribute('tabindex','#');
+            anchors[i].setAttribute('target','#');
+            anchors[i].setAttribute('rel','#');
+            console.log("[UFX] cleaned link: "+anchors[i].href);
         }
     }
 }
