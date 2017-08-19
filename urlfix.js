9d9fa1251fc793088cf06295d3f682a01675f57a
CBSkarmory <CBSkarmory@users.noreply.github.com>
2017-08-19 13:44:11 -0400

    working url cleaner on fb

--- urlfix.js
+++ urlfix.js
@@ -1,1 +1,26 @@
-document.body.style.border = "5px solid red";
+const pattern_0 = "https://l.facebook.com/l.php?u=";
+function fix_urls() {
+    var anchors = document.getElementsByTagName("a");
+    for(var i = 0; i < anchors.length; i++) {
+        if(anchors[i].href.indexOf(pattern_0) === 0) {
+            //anchors[i].onmouseover();
+            //alert("found one");
+            if(null != anchors[i].onclick) {
+                anchors[i].onmouseover();
+                //FF
+                anchors[i].setAttribute('onclick','#');
+                anchors[i].setAttribute('onmouseover','#');
+                anchors[i].setAttribute('tabindex','#');
+                anchors[i].setAttribute('target','#');
+                anchors[i].setAttribute('rel','#');
+                //IE?
+                anchors[i].onclick = function(){return;};
+                console.log("cleaned link: "+anchors[i].href);
+            }
+            
+        }
+    }
+}
+
+//run every 1 second
+setInterval(fix_urls, 1000);
