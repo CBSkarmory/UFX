6e96c276fdf980c8c87553598cd5ee2883b7fd53
CBSkarmory <CBSkarmory@users.noreply.github.com>
2017-08-20 18:31:17 -0400

    functional stuff

--- urlfix.js
+++ urlfix.js
@@ -1,7 +1,9 @@
 //exactly matches fb redirects
 const pattern_0 = "https://l.facebook.com/l.php?u=";
 //matches stuff like ?source=...
-const pattern_1 = /[^\w]source/g
+const pattern_1 = /[^\w]source/g;
+//matches sutff like utm_something=...
+const pattern_2 = "utm";
 function fix_urls() {
     var anchors = document.getElementsByTagName("a");
     for(var i = 0; i < anchors.length; i++) {
@@ -9,31 +11,31 @@ function fix_urls() {
     }
 }
 
-function filter_0(anchors, i) {
-    if(anchors[i].href.indexOf(pattern_0) === 0) {
-        if(null != anchors[i].onclick) {
-            anchors[i].onmouseover();
-            //FF
-            anchors[i].setAttribute('onclick','#');
-            anchors[i].setAttribute('onmouseover','#');
-            anchors[i].setAttribute('tabindex','#');
-            anchors[i].setAttribute('target','#');
-            anchors[i].setAttribute('rel','#');
-            anchors[i].setAttribute('href',clean_url(anchors[i].href));
-            cout("cleaned link: "+anchors[i].href);
-        }
-    }
+function filter_0(anchors) {
+    relevant = anchors.filter((anchor) => {anchors[i].href.indexOf(pattern_0) === 0});
+    relevant.forEach(function() {
+        anchors[i].onmouseover();
+        anchors[i].setAttribute('onclick','#');
+        anchors[i].setAttribute('onmouseover','#');
+        anchors[i].setAttribute('tabindex','#');
+        anchors[i].setAttribute('target','#');
+        anchors[i].setAttribute('rel','#');
+        anchors[i].setAttribute('href',clean_url(anchors[i].href));
+        cout("cleaned link: "+anchors[i].href);
+    });
 }
 
 function clean_url(url) {
-    loc_utm = url.indexOf("utm");
-    if(loc_utm != -1) {
-        url = url.substring(0,loc_utm);
-    }
     var loc_source = pattern_1.exec(url);
     if(loc_source) {
         url = url.substring(0,loc_source);
     }
+    
+    loc_utm = url.indexOf(pattern_2);
+    if(loc_utm != -1) {
+        url = url.substring(0,loc_utm);
+    }
+    
     return url;
 }
 
